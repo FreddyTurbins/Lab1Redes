598ab4bf62f9a5ad80ed056433fa48c438309450
tapsujan <simonsobenes@gmail.com>
2025-06-24 19:00:56 -0400

    init

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+
+#define int long long int
+
+signed main()
+  {
+    cout << "[DEBUG]" << endl;
+    return 0;
+  }
